47e62b515518d3a7f5bea4e4bf6d93395033547f
Patrick Miller <pjmiller823@gmail.com>
2017-03-21 15:56:18 -0400

    transitioned the second if statement to include a method.

--- guessing_game.rb
+++ guessing_game.rb
@@ -6,7 +6,9 @@ def guessed_too_low
   puts "you You guessed too low. Try again"
 end
 
-
+def guessed_too_high
+  puts "you You guessed too high. Try again"
+end
 
 loop do
   puts "Please guess a number between 1 and 100. Don't take too long"
@@ -17,7 +19,7 @@ loop do
     guessed_too_low
 
   elsif their_guess > our_number
-
+    guessed_too_high
 
   else
     puts "Congratulations! You win! You're so smart (or lucky)"
