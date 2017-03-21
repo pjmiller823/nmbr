0b93adc77d2d7338810c5d7c0ad494a759b194f5
Patrick Miller <pjmiller823@gmail.com>
2017-03-21 14:02:20 -0400

    Comments with ideas to solve all six problems

---
+++ guessing_game.rb
@@ -0,0 +1,17 @@
+#Pick a random number between 1 and 100
+#  - rand (1..100) with a variable
+
+# Prompt you for a guess
+#  - Ask to guess a number between 1 and 100
+
+# If your guess is less than the program's number, it should tell you that you were low and let you guess again.
+#   - if statement... separate from the higher than.
+
+# If the guess is greater than the program's number, it should tell you that you were high and let you guess again.
+#   -if statement again.
+
+# If your guess is correct, the program should tell you that you win and then quit.
+#   - if statement with an exit.
+
+# After 5 incorrect guesses, the program should tell you that you lose.
+#   - not really sure how to do this. add a count +1 for every if statement and if it equals 5 quit?
